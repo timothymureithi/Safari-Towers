37a7a1c27a63f3d488f2408cec65926d6a27a101
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-08-08 14:28:27 +0300

    added styling to make navbar responsive and created files for other sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <h1>Code</h1>
+      <Navbar />
   
     </div>
   );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {HiOutlineMenuAlt4} from 'react-icons/hi';
 import {FaRegTimesCircle} from 'react-icons/fa';
 import {BsFillHouseFill} from 'react-icons/bs';
 import './Navbar.css'
 
 const Navbar = () => {
+
+    const[click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
   return (
     <div className='navbar'>
       <div className='container'>
@@ -16,8 +20,10 @@ const Navbar = () => {
                     <li><a href='#'>About</a></li>
                     <li><a href='#'>Contact</a></li>
                 </ul>
-           
-
+                
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaRegTimesCircle className='icon' />) : (<HiOutlineMenuAlt4 className='icon' />)}
+        </div>
       </div>
     </div>
   )
